0ee00f9599c36dc4693dedfb870d8ad1d1b45d12
Leone & Leonessa <97951776+leoneandleonessa@users.noreply.github.com>
2023-08-20 02:03:36 +0700

    change var

--- vars.tf
+++ vars.tf
@@ -19,7 +19,7 @@ variable "birthday" {
   default = "13-08-2023"
 }
 variable "environment" {
-  default = "developmentlab2"
+  default = "development-macbook"
 }
 variable "Backup" {
   default = "BackupDaily"
